48e5a51c86758cce9c2cc71ce35b180d33d42c73
Yaroshenko Natalya <natavik2401@gmail.com>
2025-01-08 01:48:15 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,78 @@
+package ru.netology.stats;
+
+
+public class StatsService {
+
+   public int totalSales(int[] sale) {
+       int sum = 0;
+       for (int buy : sale) {    // вычисляем сумму всех продаж
+           sum = sum + buy;
+       }
+       return sum;
+
+   }
+
+    public double saleMiddleMonth(int[] sale) {
+        double sum = totalSales(sale);
+        double middleMonth = sum / 12;   // вычисляем ссреднюю сумму продаж в месяц
+
+        return middleMonth;
+
+    }
+
+
+    public int minSales(int[] sale) {
+        int minMonth = 0;
+
+        for (int i = 0; i < sale.length; i++) {
+            if (sale[i] <= sale[minMonth]) {     // номер месяца, в котором был пик продаж
+                minMonth = i;
+            }
+        }
+
+        return minMonth + 1;
+    }
+
+
+    public int maxSales(int[] sale) {
+        int maxMonth = 0;
+
+        for (int i = 0; i < sale.length; i++) {
+            if (sale[i] >= sale[maxMonth]) {     // номер месяца, в котором был минимум продаж
+                maxMonth = i;
+            }
+        }
+
+            return maxMonth + 1;
+    }
+
+
+    public int monthUnderMiddle(int[] sale) {
+        int middleSum = (int) saleMiddleMonth(sale);
+        int numberUnderMiddle = 0;
+
+        for (int i = 0; i < sale.length; i++) {   // количество месяцев, в которых продажи были ниже среднего
+            if (sale[i] < middleSum) {
+                numberUnderMiddle = numberUnderMiddle + 1;
+            }
+        }
+        return numberUnderMiddle;
+    }
+
+
+    public int monthOverMiddle(int[] sale) {
+        int middleSum = (int) saleMiddleMonth(sale);
+        int numberOverMiddle = 0;
+
+        for (int i = 0; i < sale.length; i++) {     // количество месяцев, в которых продажи были выше среднего
+            if (sale[i] > middleSum) {
+                numberOverMiddle = numberOverMiddle + 1;
+            }
+        }
+        return numberOverMiddle;
+    }
+
+
+
+
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,70 @@
+package ru.netology.stats;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class StatsServiceTest {
+
+
+    @Test
+    public void totalSales () {
+        StatsService service = new StatsService();
+
+        int[] sale = new int[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = (int) service.totalSales(sale);
+        int expected = 180;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void saleMiddleMonth () {
+        StatsService service = new StatsService();
+
+        int[] sale = new int[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        double actual = (double) service.saleMiddleMonth(sale);
+        double expected = 15;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSales () {
+        StatsService service = new StatsService();
+
+        int[] sale = new int[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = (int) service.minSales(sale);
+        int expected = 9;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSales () {
+        StatsService service = new StatsService();
+
+        int[] sale = new int[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = (int) service.maxSales(sale);
+        int expected = 8;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void monthUnderMiddle () {
+        StatsService service = new StatsService();
+
+        int[] sale = new int[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = (int) service.monthUnderMiddle(sale);
+        int expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void monthOverMiddle () {
+        StatsService service = new StatsService();
+
+        int[] sale = new int[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = (int) service.monthOverMiddle(sale);
+        int expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
